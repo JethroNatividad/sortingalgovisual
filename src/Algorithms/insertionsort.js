38abc3a15bc28a942b2253ecb54364ee03b4f313
JethroSama <kel73896@gmail.com>
2021-11-27 23:37:20 +0800

    Add insertion sort

--- src/Algorithms/insertionsort.js
+++ src/Algorithms/insertionsort.js
@@ -76,4 +76,56 @@ function selectionSort(arr) {
 }
 
 
-export default selectionSort
+function insertionSort(arr) {
+    // start from next item
+    // [2,6,4]
+    const steps = [[...arr]]
+    for (let i = 1; i < arr.length; i++) {
+        // save arr[i] to currentItem variable
+        let currentItem = arr[i];
+        let j = i - 1
+        // example i = 3 {last index of arr}
+        // compare it to 2, or i - 1 up to zero
+        while (j > -1 && arr[j] > currentItem) {
+            arr[j + 1] = arr[j]
+            steps.push([...arr])
+            j--
+
+        }
+        arr[j + 1] = currentItem
+        steps.push([...arr])
+
+
+    }
+    console.log(steps)
+    return arr
+}
+
+function insertionSort(arr) {
+    // loop arr
+    const newArr = arr.slice()
+    const steps = [[...newArr]]
+    const compareSteps = [[-1, -1]]
+    for (let i = 1; i < newArr.length; i++) {
+        // save newArr[i] to currentItem variable
+        let currentItem = newArr[i];
+        let j = i - 1
+        // example i = 3 {last index of newArr}
+        // compare it to 2, or i - 1 up to zero
+        while (j > -1 && newArr[j] > currentItem) {
+            newArr[j + 1] = newArr[j]
+            steps.push([...newArr])
+            // compareSteps.push([i, j])
+            j--
+
+        }
+        newArr[j + 1] = currentItem
+        steps.push([...newArr])
+        // compareSteps.push([i, j])
+    }
+    compareSteps.push([])
+    return { steps, compareSteps }
+}
+
+
+export default insertionSort
