68c471565434a9c59006cf2360bf739be8942e29
JethroSama <kel73896@gmail.com>
2021-12-01 18:26:38 +0800

    Add insertion sort color

--- src/Algorithms/insertionsort.js
+++ src/Algorithms/insertionsort.js
@@ -3,6 +3,8 @@ function insertionSort(arr) {
     const newArr = arr.slice()
     const steps = [[...newArr]]
     const compareSteps = [[-1, -1]]
+    steps.push([...newArr])
+    compareSteps.push([0, 1])
     for (let i = 1; i < newArr.length; i++) {
         // save newArr[i] to currentItem variable
         let currentItem = newArr[i];
@@ -10,17 +12,25 @@ function insertionSort(arr) {
         // example i = 3 {last index of newArr}
         // compare it to 2, or i - 1 up to zero
         while (j > -1 && newArr[j] > currentItem) {
+            steps.push([...newArr])
+            compareSteps.push([i, j])
+
             newArr[j + 1] = newArr[j]
             steps.push([...newArr])
-            // compareSteps.push([i, j])
+            compareSteps.push([i, j])
             j--
 
         }
-        newArr[j + 1] = currentItem
+        if (newArr[j + 1] !== currentItem) {
+            steps.push([...newArr])
+            compareSteps.push([i, j])
+
+            newArr[j + 1] = currentItem
+        }
         steps.push([...newArr])
-        // compareSteps.push([i, j])
+        compareSteps.push([i, j])
     }
-    compareSteps.push([])
+    // compareSteps.push([])
     return { steps, compareSteps }
 }
 
