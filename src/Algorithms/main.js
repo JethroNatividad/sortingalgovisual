0a732565723ad7be8ab6089672fb0ef1fa38d9b1
JethroSama <kel73896@gmail.com>
2021-12-07 23:51:04 +0800

    add mergesort to main

--- src/Algorithms/main.js
+++ src/Algorithms/main.js
@@ -1,11 +1,13 @@
 import bubbleSort from "./bubblesort";
 import insertionSort from "./insertionsort";
 import selectionSort from "./selectionsort";
+import mergeSort from "./mergesort";
 
 const main = {
     'bubblesort': bubbleSort,
     'selectionsort': selectionSort,
-    'insertionsort': insertionSort
+    'insertionsort': insertionSort,
+    'mergesort': mergeSort,
 }
 
 export default main
