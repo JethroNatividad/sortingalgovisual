2e88528d27519b0155bcda4b0044c391c394815a
JethroSama <kel73896@gmail.com>
2021-12-08 23:53:43 +0800

    cleanup bar.js

--- src/Bar.js
+++ src/Bar.js
@@ -7,16 +7,7 @@ const Bar = ({ num, color }) => {
         num
     }
     const classes = useStyles(styleProps)
-    // const styles = {
-    //     height: num + 30 + 'px',
-    //     width: '30px', background: color,
-    //     margin: '0px 5px',
-    //     color: 'white',
-    //     display: 'flex',
-    //     alignItems: 'end',
-    //     justifyContent: 'center',
-    //     paddingBottom: '10px'
-    // }
+
 
     return (
         <div className={classes.main}>
